2d46b033b9e656cfede1b81d0c14af0d5d425c5f
likhith00 <likhith.prudhivi@gmail.com>
2021-11-16 13:39:34 +0530

    confusion matrix added

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -26,7 +26,7 @@ def train_and_evaluate(config_path):
     y_test  = test_data[target]
     reports = config["reports"]["scores"]
 
-    mlflow_config = config["mlflow_config"]
+    '''mlflow_config = config["mlflow_config"]
     remote_server_uri = mlflow_config["remote_server_uri"]
 
     mlflow.set_tracking_uri(remote_server_uri)
@@ -34,32 +34,33 @@ def train_and_evaluate(config_path):
     mlflow.set_experiment(mlflow_config["experiment_name"])
 
     
-    with mlflow.start_run(run_name=mlflow_config["run_name"]) as mlops_run:
-        knc = KNeighborsClassifier(n_neighbors= neighbors)
-        knc.fit(X_train,y_train)
-        y_pred = knc.predict(X_test)
-        score = accuracy_score(y_test,y_pred)
-        plot_confusion_matrix(knc, X_test, y_test)
-        plt.savefig('confusion_matrix.png')
+    with mlflow.start_run(run_name=mlflow_config["run_name"]) as mlops_run:'''
+    knc = KNeighborsClassifier(n_neighbors= neighbors)
+    knc.fit(X_train,y_train)
+    y_pred = knc.predict(X_test)
+    score = accuracy_score(y_test,y_pred)
 
-        j_score = {"accuracy":score}
-        with open(reports,"w") as json_file:
-            json.dump(j_score,json_file,indent=4)
-        os.makedirs(model_dir, exist_ok=True)  
-        model_path = os.path.join(model_dir, "model.joblib")
-        joblib.dump(knc,model_path)
-        mlflow.log_param("neighbors", neighbors)
-        mlflow.log_metric("accuracy", score)
+    plot_confusion_matrix(knc, X_test, y_test)
+    plt.savefig('confusion_matrix.png')
 
-        tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
+    j_score = {"accuracy":score}
+    with open(reports,"w") as json_file:
+        json.dump(j_score,json_file,indent=4)
+    os.makedirs(model_dir, exist_ok=True)  
+    model_path = os.path.join(model_dir, "model.joblib")
+    joblib.dump(knc,model_path)
+    '''mlflow.log_param("neighbors", neighbors)
+    mlflow.log_metric("accuracy", score)
 
-        if tracking_url_type_store != "file":
-            mlflow.sklearn.log_model(
-                knc, 
-                "model", 
-                registered_model_name=mlflow_config["registered_model_name"])
-        else:
-            mlflow.sklearn.load_model(knc, "model")
+    tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
+
+    if tracking_url_type_store != "file":
+        mlflow.sklearn.log_model(
+            knc, 
+            "model", 
+            registered_model_name=mlflow_config["registered_model_name"])
+    else:
+        mlflow.sklearn.load_model(knc, "model")'''
 
 
 
